050e17004143eacc7d3fb259bc21f016d6468f7b
jwhgogogo82 <jwhgogogo82@gmail.com>
2022-06-08 01:15:11 +0900

    ins

--- zaum-front/src/App.vue
+++ zaum-front/src/App.vue
@@ -4,7 +4,12 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <p class="text-lg"></p>
+  <div class="container mx-auto w-full text-center p-20 ">
+    <p class="m-10 font-bold subpixel-antialiased text-6xl font-sans text-black">접속테스트</p>
+    <button class="border-1 border-black shadow-2xl rounded-lg text-2xl bg-cyan-500 hover:bg-cyan-700 p-3 m-10 pl-10 pr-10 text-white" >ㄱㄱ</button>
+    <button class="border-1 border-black shadow-2xl rounded-lg text-2xl bg-cyan-500 hover:bg-cyan-700 p-3 m-10 pl-10 pr-10 text-white" >ㄴㄴ</button>
+  </div>
+  
 </template>
 
 <style>
