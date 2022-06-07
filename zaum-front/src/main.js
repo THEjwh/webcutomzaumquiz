57c61f2872844d635a3f05bcdd25fcf132abe540
jwhgogogo82 <jwhgogogo82@gmail.com>
2022-06-07 23:57:37 +0900

    setup

--- zaum-front/src/main.js
+++ zaum-front/src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as Colyseus from "colyseus.js";
+import axios from 'axios'
+import vueAxios from 'vue-axios'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vueAxios, axios)
+app.config.globalProperties.$colyseus = Colyseus
 
 app.mount('#app')
