74b2fbda17288e713a7a3bcc2d1448d979a268ca
jwhgogogo82 <jwhgogogo82@naver.com>
2022-06-12 17:59:08 +0900

    error fix

--- zaum-front/src/components/Lobby.vue
+++ zaum-front/src/components/Lobby.vue
@@ -8,7 +8,7 @@ export default{
             mods: [
                 {name:'선착순 1명', description: '선착순 1명만 점수를 얻습니다.'}
             ],
-            selected_Mod
+            selected_Mod : undefined
         }
     },
     setup(props) {
