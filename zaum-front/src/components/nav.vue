babac612daf4dec949be02f946d4e1a9c6370188
SyrupJH <soip17@naver.com>
2022-06-12 18:18:37 +0900

    커밋테스트

--- zaum-front/src/components/nav.vue
+++ zaum-front/src/components/nav.vue
@@ -3,7 +3,7 @@
       <div class="flex content-start">
         <p
           class="m-4 flex-initial text-black font-sans text-2xl antialiased font-bold tracking-wide text-center align-middle hover:opacity-50 ">
-          메인
+          메인화면
         </p>
       </div>
     </nav>
