c94a2d1c60f2b09faecdcb61e53b5d9fd66fc91e
jwhgogogo82 <jwhgogogo82@naver.com>
2022-06-16 23:49:14 +0900

    else

--- zaum-front/src/components/nav.vue
+++ zaum-front/src/components/nav.vue
@@ -3,11 +3,11 @@
       <div class="flex content-start">
         <p
           class="m-4 flex-initial text-black font-sans text-2xl antialiased font-bold tracking-wide text-center align-middle hover:opacity-50 p-2 ">
-          메인
+          메인(장식)
         </p>
         <p
           class="m-4 flex-initial text-black font-sans text-2xl antialiased font-bold tracking-wide text-center align-middle hover:opacity-50 bg-slate-100 p-2 rounded-xl">
-          단어장 에디터(희망사항)
+          단어장 에디터(언젠가만듬)
         </p>
       </div>
     </nav>
