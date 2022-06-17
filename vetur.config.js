0e963911d41e4e84b059a780d4391f55ac1071cd
jwhgogogo82 <jwhgogogo82@naver.com>
2022-06-17 21:03:03 +0900

    asdff

---
+++ vetur.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // **optional** default: `{}`
+    // override vscode settings
+    // Notice: It only affects the settings used by Vetur.
+    settings: {
+      "vetur.useWorkspaceDependencies": true,
+      "vetur.experimental.templateInterpolationService": true
+    },
+    // **optional** default: `[{ root: './' }]`
+    // support monorepos
+    projects: [
+      './zaum', // Shorthand for specifying only the project root location
+      {
+        // **required**
+        // Where is your project?
+        // It is relative to `vetur.config.js`.
+        root: './zaum-front',
+        package: './package.json',
+
+      }
+    ]
+  }
---
+++ zaum-front/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import {provide, inject, onMounted, ref } from 'vue'
+import Nav from './components/nav.vue'
+import Footer from './components/footer.vue';
+export default {
+    setup() {
+        const client = ref();
+        const room = ref(undefined);
+        const url = ref("localhost:2567")
+        provide("client", client);
+        provide("room", room);
+        provide('url', url)
+    },
+    components: { Nav, Footer } //testasdf
+}
+</script>
+
+<template>
+  <div class="w-scrren h-screen bg-slate-50">
+    <Nav></Nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
