ae90713aabcebb2719519b2a4d186a5216d93bda
jwhgogogo82 <jwhgogogo82@naver.com>
2022-06-17 21:33:37 +0900

    갑자기 꼬여버린 git을 싹다 밀어버리고... test 브랜치가 새 기준이 되다

--- zaum-front/src/components/nav.vue
+++ zaum-front/src/components/nav.vue
@@ -7,7 +7,7 @@
         </p>
         <p
           class="m-4 flex-initial text-black font-sans text-2xl antialiased font-bold tracking-wide text-center align-middle hover:opacity-50 bg-slate-100 p-2 rounded-xl">
-          단어장 에디터(언젠가만듬)
+          단어장 에디터(곧만듬)
         </p>
       </div>
     </nav>
